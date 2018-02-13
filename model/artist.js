56817e001fb54d9654d945584351809b16b04fb7
Jamie Williams <jwilliams492@gmail.com>
2018-02-13 10:54:22 -0800

    complete development branch

---
+++ model/artist.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const Artist= mongoose.Schema({
+  name: {type: String, required: true},
+  album_ids: [{type: mongoose.Schema.Types.ObjectId, ref: 'album'}],  
+},
+{timestamps: true}
+);
+
+module.exports = mongoose.model('artist', Artist);
