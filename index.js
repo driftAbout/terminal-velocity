08b9382fe8f814ddcf76f4b28e5c41c276fd3947
Jeremy <jpjazzy@hotmail.com>
2018-02-13 17:27:23 -0800

    added comments to show where to do superagent call when working

--- index.js
+++ index.js
@@ -4,3 +4,9 @@
 const songPlayer = require('./lib/song-player');
 
 
+//collection.import needs to return structure:
+// {
+//   Artists:[{Artist Schema Object}, {Artist Schema Object}, {Artist Schema Object}, ...],      FOLDERS IN SPECIFIED DIR (TOP LEVEL)
+//   Albums:[{Album Schema Object}, {Album Schema Object}, {Album Schema Object}, ...],          FOLDERS IN ARTIST FOLDERS (2ND LEVEL)
+//   Tracks:[{Track Schema Object}, {Track Schema Object}, {Track Schema Object}, ...]           MP3 FILES IN ALBUM FOLDERS (3RD LEVEL)
+// }
--- route/route-import.js
+++ route/route-import.js
@@ -2,10 +2,13 @@
 
 const bodyParser = require('body-parser').json();
 const errorHandler = require('../lib/error-handler');
+const collection = require('../lib/collection');
 
 module.exports = function(router) {
   router.post('/import', bodyParser, (req, res) => {
     //use collection setup to generate tree
-    //
+    //save all artist schemas to db
+    //save all album schemas to db
+    //save all track schemas to db
   });
 };
