97bb83dd643293edb3825f465ecf748963a557c0
Jordan Van Ness <jordanwvn@gmail.com>
2018-02-14 12:59:03 -0800

    collection.js is a module to parse a user's directory. Create a new collection with the root directory as an argument, and then call <collection name>.create(<collection name>.rootName, <collection name>.rootPath). It will output arrays of artist, album, and track data to a json file at /import-data/collection.json.

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 'use strict';
 
 const Queue = require('./lib/queue.js');
+const Stack = require('./lib/stack.js');
 const Collection = require('./lib/collection.js');
+// const newCollection = require('./lib/iterativeCollection.js');
 
-const rootDirectory = './music';
-// const rootDirectory = '/home/jordan/Music';
+// const rootDirectory = './music';
+const rootDirectory = '/home/jordan/Music';
 
 let collection = new Collection (rootDirectory);
+let history = new Stack ();
+let queue = new Queue (history);
 
-collection.import(collection.root);
+
+// newCollection('music', rootDirectory).then(console.log);
+
+collection.create(collection.rootName, collection.rootPath);
+// console.log(collection.create(collection.rootName, collection.rootPath));
+
 // console.log("tree", tree); // NOTE needs to be called asynchronously
 
-// let queue = new Queue ();
-// queue.enqueue('./music/1.mp3');
-// queue.enqueue('./music/1.mp3');
+// queue.enqueue(collection.children[0].children[0].children[0]);
+// queue.enqueue('./music/King Gizzard & The Lizard Wizard/Polygondwanaland/01 - Crumbling Castle.mp3');
+// queue.enqueue('./music/King Gizzard & The Lizard Wizard/Polygondwanaland/02 - Polygondwanaland.mp3');
 // queue.dequeue();
