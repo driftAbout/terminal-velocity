612fa5958f652fe8f1d2484e7162946309696ad3
Kevin Miller <kevin@driftabout.com>
2018-02-14 16:26:20 -0800

    added route-album and basic test file

---
+++ route/route-album.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const Track = require('../model/track');
+const bodyParser = require('body-parser').json();
+const errorHandler = require('../lib/error-handler');
+const debug = require('debug')('http:route-play');
+
+
+module.exports = function (router) {
+
+  router.route('/play/album/:artist/:album')
+    .get((req, res) => {
+      let {artist, album} = req.params; 
+      Track.find({artist_name: artist, album_title: album})
+        .then(tracks => {
+          if (!tracks.length) return Promise.reject(new Error('Error ENOENT: Requested resource not found'));
+          res.status(200).json(tracks);
+        })
+        .catch(err => errorHandler(err, res));
+    });  
+};
