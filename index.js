8e7ae2d855b27563dfd5bf471cb88051e5a8f127
Jeremy <jpjazzy@hotmail.com>
2018-02-15 08:26:21 -0800

    played with the playlist

--- index.js
+++ index.js
@@ -2,15 +2,6 @@
 
 const server = require('./lib/server');
 const songPlayer = require('./lib/song-player');
-const Queue = require('./lib/queue');
-
-let myQueue = new Queue();
-myQueue.enqueue('/home/jeremy/Music/pseudo-artist2/Some-album2/1.mp3');
-myQueue.enqueue('/home/jeremy/Music/pseudo-artist2/Some-album2/1.mp3');
-myQueue.enqueue('/home/jeremy/Music/pseudo-artist2/Some-album2/1.mp3');
-myQueue.enqueue('/home/jeremy/Music/pseudo-artist2/Some-album2/1.mp3');
-console.log('My Dequeue:', myQueue.dequeue().path)
-
 
 server.start();
 
